672a39b70e012769960ea39c67e01c368775d8b8
Christopher Teubert <christopher.a.teubert@nasa.gov>
2018-03-04 19:34:35 -0800

    Added CO2 impact

--- Chris_Database/app/src/main/java/edu/scu/databaseexample/FootprintEstimate.java
+++ Chris_Database/app/src/main/java/edu/scu/databaseexample/FootprintEstimate.java
@@ -20,6 +20,80 @@ public class FootprintEstimate {
 
     public long nDays;
 
+    public static FootprintEstimate generateEstimate(DayTripsSummary dayTrips, UserProfile user) {
+        double co2 = 0;
+
+        // TODO(CT): FIX UNITS- KG
+
+        // http://shrinkthatfootprint.com/what-is-your-carbon-footprint
+
+        // Add trips portion
+        for (Trip trip : dayTrips.trips) {
+            co2 += trip.getEstimate().CO2;
+        }
+
+        // Add breathing
+        // From http://www.slate.com/articles/news_and_politics/explainer/2009/08/7_billion_carbon_sinks.html
+        co2 += 2.3;
+        // TODO(CT): Add pets
+
+        // Add food
+        // 	= Σ[CO2 from farm] + Σ[CO2 from transport]
+        //  - Transport Decreased through farmers market
+        //  - Farm decreased by going vegetarian/vegan
+
+        // CO2 Emissions: http://www.greeneatz.com/foods-carbon-footprint.html - does not include freight
+        // Only production
+        final double mealCO2            = 9.04; // kg/day
+        final double averageFoodCO2     = 6.85; // kg/day
+        final double noBeefCO2          = 5.25; // kg/day
+        final double vegitarianFoodCO2  = 4.66; // kg/day
+        final double veganFoodCO2       = 4.11; // kg/day
+
+        // https://www.npr.org/sections/thesalt/2011/12/31/144478009/the-average-american-ate-literally-a-ton-this-year
+        final double food_consumption   = 2.4787; // kg food/day
+
+        // Freight
+        // https://carbonfund.org/how-we-calculate/
+        final double air_cargo          = 8.196e-7;     // kg CO2/kg-km
+        final double truck              = 9.1e-8;       // kg CO2/kg-km
+        final double train              = 1.5e-8;       // kg CO2/kg-km
+        final double sea                = 3.741e-8;     // kg CO2/kg-km
+
+        // https://cuesa.org/learn/how-far-does-your-food-travel-get-your-plate
+        final double far_distance       = 3500;      // km - guess
+        final double average_distance   = 2414;      // km - how far food travels
+        final double local_distance     = 125;       // km
+
+        final double freight_far        = far_distance * (air_cargo + train + sea)/3; // average
+        final double freight_average    = average_distance * (air_cargo + train + sea)/3; // average
+        final double freight_local      = local_distance * truck;  // local typically truck
+
+        co2 += averageFoodCO2 + freight_average;
+
+        // Add electricity
+        //= Σ[Source efficiency]*[Percentage from source]*[Total Amount]
+       // - Decreased through more efficient sources (get solar panel, etc)
+        // - Decreased by degrading amount
+        final double lbsPerKWh = 1.222;
+        final double averageKWh = 29.4757; // from: https://www.eia.gov/tools/faqs/faq.php?id=97&t=3
+        co2 += averageKWh*lbsPerKWh;
+
+        // TODO(CT): Add locality for power source/average house use
+        // TODO(CT): Add actual energy use
+        // TODO(CT): Add house size
+
+        // Add other sources
+        double other = 0;
+        co2 += other;
+
+        // Reductions
+        double reductions  = 0;
+        co2 -= reductions;
+
+        return new FootprintEstimate(co2, 1);
+    }
+
     /**
      * Create an estimate
      *
