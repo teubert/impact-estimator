e3c374ffc4b6f19771fb44b0fe1c156f9d8c463a
Christopher Teubert <christopher.a.teubert@nasa.gov>
2018-03-07 20:07:47 -0800

    Logic to write GPS to database

--- Final_project/app/src/main/java/com/coen/scu/final_project/activity/HomeActivity.java
+++ Final_project/app/src/main/java/com/coen/scu/final_project/activity/HomeActivity.java
@@ -1,15 +1,24 @@
 package com.coen.scu.final_project.activity;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,16 +32,77 @@ import com.coen.scu.final_project.R;
 import com.coen.scu.final_project.fragment.ProfileFragment;
 import com.coen.scu.final_project.fragment.RankingFragment;
 import com.coen.scu.final_project.fragment.SummaryFragment;
+import com.coen.scu.final_project.java.GPSPath;
+import com.coen.scu.final_project.java.GPSPoint;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.time.LocalDate;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private final String DEBUG_TAG = "HomeActivity";
+
     private FirebaseAuth mAuth;
+    private LocationManager lm;
+    private String id;
+
+    /**
+     *  Start the location connection
+     */
+    void startConnection() {
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 10, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    GPSPoint gpsPoint = new GPSPoint(location.getTime(), location.getLongitude(), location.getLatitude());
+                    gpsPoint.speed = location.getSpeed();
+                    Log.v(DEBUG_TAG, String.format("LocationListener: Writing position update to database: %f, %f %f", gpsPoint.lat, gpsPoint.lon, gpsPoint.speed));
+                    GPSPath.addNewGPSDataPoint(id, gpsPoint);
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+            Log.w(DEBUG_TAG, "startConnection: Missing required permissions (FINE_LOCATION)");
+        }
+    }
+
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        id = firebaseUser.getUid();
+        startConnection();
+
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -69,7 +139,6 @@ public class HomeActivity extends AppCompatActivity
                 Toast.makeText(HomeActivity.this, "failed", Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             }
-
         }
 
         if(isOldUser){
@@ -83,11 +152,12 @@ public class HomeActivity extends AppCompatActivity
                 Toast.makeText(HomeActivity.this, "failed", Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             }
-
         }
-
     }
 
+    /**
+     *
+     */
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -102,6 +172,11 @@ public class HomeActivity extends AppCompatActivity
         }
     }
 
+    /**
+     *
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -109,6 +184,9 @@ public class HomeActivity extends AppCompatActivity
         return true;
     }
 
+    /**
+     *
+     */
     @Override
     protected void onStart() {
         super.onStart();
@@ -120,7 +198,11 @@ public class HomeActivity extends AppCompatActivity
 
     }
 
-
+    /**
+     *
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -138,6 +220,9 @@ public class HomeActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     *
+     */
     private void sendToSart() {
         Intent intent = new Intent(HomeActivity.this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -145,6 +230,11 @@ public class HomeActivity extends AppCompatActivity
         finish();
     }
 
+    /**
+     *
+     * @param item
+     * @return
+     */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -188,6 +278,11 @@ public class HomeActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    /**
+     *
+     * @param title
+     */
     public void setActionBarTitle(String title) {
         getSupportActionBar().setTitle(title);
     }
